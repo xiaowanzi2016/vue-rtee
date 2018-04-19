ab8de558036597c5cf66eed95602e6cef649e10a
woshixiaowanzi2016 <1650426200@qq.com>
2018-04-19 20:48:11 +0800

    tree

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div  id="app">
+    <ul>
+      <tree-menu     v-for="(item,index) in tree "  :data="item" :key="index" ></tree-menu>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TreeMenu from './components/HelloWorld'
+var data = [
+  {
+    lable: 'root',
+    nodes: [
+      {
+        lable: 'item1',
+        nodes: [
+          {
+            lable: 'item1.1'
+          },
+          {
+            lable: 'itemm1.2',
+            nodes: [
+              {
+                lable: 'item1.2.1'
+              }
+            ]
+
+          }
+        ]
+
+      },
+      {
+        lable: 'item2'
+      }
+    ]
+  }
+]
+
+export default{
+  data: function () {
+    return {
+      tree: data
+    }
+  },
+  components: {
+    TreeMenu
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tree">
+      <li >
+        <span @click="toggle" >
+          <i class="fa" :class="data.nodes && data.nodes.length ? 'fa-align-center': 'fa fa-file-o' "></i>
+          {{data.lable}}
+        </span>
+      </li>
+      <ul>
+        <tree-menu v-show="open"  v-for="(item,index) in data.nodes " :data="item" :key="index"></tree-menu>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+  props: ['data'],
+  name: 'tree-menu',
+  data: function () {
+    return {
+      theFold: [],
+      open: false
+
+    }
+  },
+  methods: {
+    toggle: function () {
+      this.open = !this.open
+    }
+  }
+}
+</script>
+<style>
+ul{
+  list-style: none;
+}
+</style>
